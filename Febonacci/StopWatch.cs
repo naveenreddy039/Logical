8ad4836b8e00d51200b2e6251fdef8fc6c14c058
naveen <naveenreddy.k07@gmail.com>
2021-11-06 20:05:46 +0530

    second commit

--- Febonacci/StopWatch.cs
+++ Febonacci/StopWatch.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Diagnostics;
+using System.Threading;
 
 namespace Febonacci
 {
@@ -9,21 +10,30 @@ namespace Febonacci
     {
         public void printWatch()
         {
-            Console.WriteLine("Enter the start to start the stopwatch:");
-            String str1 = Console.ReadLine();
-            StopWatch watch= null;
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            Console.WriteLine("enter number:");
+            for(int i=0;i<1000;i++)
+            {
+                Thread.Sleep(12);
+            }
+            stopwatch.Stop();
+            Console.WriteLine("elast time:{0:hh\\:mm\\:ss}",stopwatch.Elapsed);
+            ////Console.WriteLine("Enter the start to start the stopwatch:");
+            //String str1 = Console.ReadLine();
+            //StopWatch watch= null;
 
-            if(str1.Equals("start"))
-            {
-                new StopWatch();
-                //watch.Start();
-            }
-            Console.WriteLine("enter the end to end the stopwatch");
-            String str2 = Console.ReadLine();
-            if(str2.Equals("stop"))
-            {
-                //watch.Stop();
-            }
+            //if(str1.Equals("start"))
+            //{
+            //    new StopWatch();
+            //    watch.Start();
+            //}
+            //Console.WriteLine("enter the end to end the stopwatch");
+            //String str2 = Console.ReadLine();
+            //if(str2.Equals("stop"))
+            //{
+            //    watch.Stop();
+            ////}
         }
     }
 }
